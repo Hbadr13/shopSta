04eddabb1540a1d0bfd7ef01314027e86fd9a61e
Hamza Badr <hamza1937badr@gmail.com>
2025-03-10 13:40:34 +0000

    none

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -83,6 +83,8 @@ export const loginUser = async (request: Request, response: Response) => {
     );
     response.cookie("token", token,
         {
+
+
             httpOnly: true,
             sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
             maxAge: 7 * 24 * 60 * 60 * 1000,
