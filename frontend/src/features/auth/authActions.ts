5108bd8da1e88d99e6aec82eb072827034522ef1
hbadr13 <hamza1937badr@gmail.com>
2025-03-11 11:52:36 +0000

    fetch2

--- frontend/src/features/auth/authActions.ts
+++ frontend/src/features/auth/authActions.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "@/lib/axiosInstance";
 import { handleAxiosError } from "@/lib/axiosInstanceError";
